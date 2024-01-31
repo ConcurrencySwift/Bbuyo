9516c964f68fd34d441b54b8dabc2203b23235e7
naldal <https://github.com/naldal>
2024-01-31 23:07:22 +0900

    🚧 랜덤숫자 소켓 send 구현

--- W03/nodeServer/websocketServer.js
+++ W03/nodeServer/websocketServer.js
@@ -1,23 +1,41 @@
 const WebSocket = require('ws');
-
 const wss = new WebSocket.Server({ port: 3001 });
 
-wss.on('connection', function connection(ws) {
-  console.log('Client connected');
+function sendRandomNumbers(ws) {
+  let count = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+  let numbersSent = 0;
+  let sum = 0;
+  let delayBetweenMessages = 300;
 
-  const sendRandomNumber = () => {
+  const sendNumber = () => {
     if (ws.readyState === WebSocket.OPEN) {
-      const randomNumber = Math.floor(Math.random() * 100); // 0에서 99 사이의 랜덤 숫자
-      ws.send(randomNumber.toString());
+      if (numbersSent < count) {
+        const randomNumber = Math.floor(Math.random() * 101);
+        ws.send(randomNumber.toString());
+        sum += randomNumber;
+        numbersSent++;
+        setTimeout(sendNumber, delayBetweenMessages);
+      } else {
+        ws.send(`Sum: ${sum}`);
+        setTimeout(() => {
+          numbersSent = 0;
+          sum = 0;
+          count = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+          sendNumber(); 
+        }, 1000);
+      }
     }
   };
 
-  // 100ms마다 랜덤 숫자 전송 (초당 10번)
-  const intervalId = setInterval(sendRandomNumber, 100);
+  sendNumber();
+}
+
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
+  sendRandomNumbers(ws);
 
   ws.on('close', function close() {
     console.log('Client disconnected');
-    clearInterval(intervalId); // 클라이언트 연결이 끊어지면 인터벌을 멈춤
   });
 });
 
